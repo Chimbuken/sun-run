38f9817f61a53c35fcd3081f863e061e2cc54192
AdrienneLee <adrienneklee@gmail.com>
2021-11-27 14:07:37 -0500

    added axios call to sunrise api

--- src/components/RunSetup.js
+++ src/components/RunSetup.js
@@ -7,10 +7,14 @@ function RunSetup() {
     pace :'Jog',
     distance: '5km',
     date: `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`,
-    sunrise: false,
-    sunset: false
+    userSunrise: false,
+    userSunset: false,
+    lat: 43.64829, 
+    lng: -79.39785
   })
   
+  const [runResults, setRunResults] = useState({})
+
   const handleChange = (e)=>{
     const {id, value} = e.target;
   
@@ -21,16 +25,64 @@ function RunSetup() {
     e.preventDefault()
 
     const {id} = e.target;
-    if(id === "sunrise"){
-      setFirstRun({...firstRun, sunrise:true, sunset: !firstRun.sunset})
+    if(id === "userSunrise"){
+      setFirstRun({...firstRun, userSunrise:true, userSunset:false})
     }else {
-      setFirstRun({...firstRun, sunrise:! firstRun.sunrise, sunset: true})
+      setFirstRun({...firstRun, userSunrise:false, userSunset: true})
     }
-
   }
-  const setRun = (e)=>{
+  const setRun = (e)=> {
+ 
     e.preventDefault()
     console.log(firstRun)
+// https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+    axios({
+      url: `https://api.sunrise-sunset.org/json?lat=${firstRun.lat}=${firstRun.lng}=today`,
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        lat: firstRun.lat,
+        lng: firstRun.lng
+      }
+    }).then((response) => {
+      console.log(response.data.results)
+      const something = response.data.results.sunrise;
+      console.log('something: ', something)
+
+      const event = new Date();
+      // expected output: Wed Oct 05 2011 16:48:00 GMT+0200 (CEST)
+      // (note: your timezone may vary)
+
+      // something:  12:25:27 PM
+
+      // new Date('05 October 2011 14:48 UTC');
+      
+      const isoDate= event.toISOString()
+      console.log("isoDate: ", isoDate);
+
+      setRunResults({...response.data.results, ...firstRun});
+      
+      // if(firstRun.pace === "Jog" ){
+      //   if(firstRun.distance === "5km"){
+      //           {/* 
+      //   if user chooses jog - 5k - 
+      //   mid point: sunrise
+      //   total time 37.5minutes
+      //   runResults.sunrise -  37.5/2
+
+      // */}
+      //     const totalRunTime = 37.5
+      //     const startTime = totalRunTime/
+      //   }
+
+      // }
+      // if(firstRun.pace === "Run" ){
+
+      // }
+    
+
+
+    });
   }
   return (
     <section>
@@ -62,12 +114,30 @@ function RunSetup() {
           min={firstRun.date}/>
         </div>
         <div>
-          <button id="sunrise" onClick={handleTimeClick}>Sunrise</button>
-          <button id="sunset" onClick={handleTimeClick}>Sunset</button>
+          <button id="userSunrise" onClick={handleTimeClick}>Sunrise</button>
+          <button id="userSunset" onClick={handleTimeClick}>Sunset</button>
         </div>
 
-        <button onSubmit={setRun}>Submit</button>
+        <button onSubmit={setRun}>Set</button>
       </form>
+
+      <div className='runResults'>
+        <h3>Here's what we got for you:</h3>
+
+        {
+          runResults ?
+          <>
+          {runResults.userSunrise === true ? <p>sunrise: {runResults.sunrise}</p> : null}
+          {/* {runResults.userSunrise === true ?
+            runResults.pace ==='Jog'  ?
+           <p>sunrise: {runResults.sunrise-}</p> 
+           
+           : null} */}
+          {runResults.userSunset === true ? <p>sunset: {runResults.sunset}</p> : null}
+          </>
+          : null
+        }
+      </div>
 
 
     </section>
