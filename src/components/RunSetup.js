ff6ebdad89745c310b73c0e6028a3dc31341e08a
SaraMunir <sara.m07011@gmail.com>
2021-11-27 21:19:57 -0500

    changed the api call to include date parameter as well

--- src/components/RunSetup.js
+++ src/components/RunSetup.js
@@ -4,33 +4,24 @@ import axios from 'axios';
 function RunSetup() {
   const month= ["January","February","March","April","May","June","July",
   "August","September","October","November","December"];
-
   const d = new Date();
+  const [alert, setAlert] = useState({alert: false, alertMessage:''})
   const [firstRun, setFirstRun] = useState({
     pace :'Jog',
     distance: '5km',
-    date: `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`,
+    date: `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`, //be default the value will be the current date
     userSunrise: false,
     userSunset: false,
     lat: 43.64829, 
     lng: -79.39785,
-    year: d.getFullYear(),
-    month: d.getMonth(),
-    day: d.getDate()
   })
-  
   const [runResults, setRunResults] = useState({})
-
   const handleChange = (e)=>{
     const {id, value} = e.target;
-  
     setFirstRun({...firstRun, [id]:value})
-
   }
   const handleTimeClick = (e)=>{
     e.preventDefault()
-    
-
     const {id} = e.target;
     if(id === "userSunrise"){
       setFirstRun({...firstRun, userSunrise:true, userSunset:false})
@@ -42,105 +33,111 @@ function RunSetup() {
 
     e.preventDefault()
     console.log(firstRun)
-    
-// https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
-    axios({
-      url: `https://api.sunrise-sunset.org/json?lat=${firstRun.lat}=${firstRun.lng}=today`,
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        lat: firstRun.lat,
-        lng: firstRun.lng
-      }
-    }).then((response) => {
-      console.log(response.data.results)
-      // getting the values for sunrise and sunset from api call. 
-      const sunrise = response.data.results.sunrise;
-      const sunset = response.data.results.sunset;
-    
-    
-      // converting the results in to utc format so that we can convert it to local time. 
-      const utcSunrise = `${firstRun.day} ${month[firstRun.month]} ${firstRun.year} ${sunrise} UTC`
-      const utcSunset= `${firstRun.day} ${month[firstRun.month]} ${firstRun.year} ${sunset} UTC`
-
+    if(firstRun.userSunrise ===false && firstRun.userSunset === false){
+      console.log('please select at least sunrise or sunset')
+      setAlert({alert: true, alertMessage:'Please atleast select sunrise or sunset'})
+    } else{
+      setAlert({alert: false, alertMessage:''})
+      // https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+      axios({
+        url: `https://api.sunrise-sunset.org/json?lat=${firstRun.lat}=${firstRun.lng}=${firstRun.date}`,
+        method: "GET",
+        dataResponse: "json",
+        params: {
+          lat: firstRun.lat,
+          lng: firstRun.lng
+        }
+      }).then((response) => {
+        console.log(response.data.results)
+        // getting the values for sunrise and sunset from api call. the results we receive from the api call only provide time 
+        const sunrise = response.data.results.sunrise;
+        const sunset = response.data.results.sunset;
+  
+        // getting the values of date to convert the time from utc to local time
+        const selectedDateArray =  firstRun.date.split("-")
+        console.log('dateArray: ', selectedDateArray)
+        const selectedYear = selectedDateArray[0]
+        const selectedMonth = selectedDateArray[1]
+        const selectedDate = selectedDateArray[2]
       
-      // converting the utc time to local time which gives us a sting of date , time and year and time zone. 
-      let localSunriseTime = new Date(utcSunrise).toString()
-      let localSunsetTime = new Date(utcSunset).toString()
+        // converting the results in to utc format so that we can convert it to local time. 
+        const utcSunrise = `${selectedDate} ${month[selectedMonth-1]} ${selectedYear} ${sunrise} UTC`
+        const utcSunset= `${selectedDate} ${month[selectedMonth-1]} ${selectedYear} ${sunset} UTC`
+        
+        // converting the utc time to local time which gives us a sting of date , time and year and time zone. 
+        let localSunriseTime = new Date(utcSunrise).toString()
+        let localSunsetTime = new Date(utcSunset).toString()
+  
+        // converting the strings into array to get the time for now. 
+        const sunriseArray = localSunriseTime.split(" ")
+        const sunsetArray = localSunsetTime.split(" ")
+        
+        localSunriseTime =  sunriseArray[4]
+        localSunsetTime =  sunsetArray[4]
+        let totalTime
+        
+        if(firstRun.pace ==="Jog" ){
+          if(firstRun.distance === '5km'){
+            totalTime = 37.5
+          }
+          if(firstRun.distance === '10km'){
+            totalTime = 75
+          }
+          if(firstRun.distance === 'Half Marathon'){
+            totalTime = 157.5
+          }
+          if(firstRun.distance === 'Marathon'){
+            totalTime = 315
+          }
+          // firstRun.distance
+        }
+        if(firstRun.pace ==="Run"){
+          if(firstRun.distance === '5km'){
+            totalTime = 18.75
+          }
+          if(firstRun.distance === '10km'){
+            totalTime = 37.5
+          }
+          if(firstRun.distance === 'Half Marathon'){
+            totalTime = 78.75
+          }
+          if(firstRun.distance === 'Marathon'){
+            totalTime = 157.5
+          }
+        }
+        // 
+        let timeInMinutes
+        if(firstRun.userSunrise){
+          timeInMinutes = convertH2M(localSunriseTime);
+        }else if(firstRun.userSunset){
+          timeInMinutes = convertH2M(localSunsetTime);
+        }
+  
+        const startTime = Number(timeInMinutes) -(totalTime/2)
+        let startMinute = Math.floor(startTime % 60)
+        let startHour = (Math.floor(startTime / 60 ))
+        if(startMinute < 10){
+          startMinute = `0${startMinute}`
+        }
+        let convertingToHourMinute
+        if(startHour > 12){
+          convertingToHourMinute = (`${startHour-12}:${startMinute} PM`)
+        }else {
+          convertingToHourMinute = (`${startHour}:${startMinute} AM`)
+        }
+  
+        localSunriseTime =  sunriseArray[4]
+        localSunsetTime =  sunsetArray[4]  
+        setRunResults({...firstRun, sunsetData: localSunsetTime, sunriseData:localSunriseTime, departureTime: convertingToHourMinute });   
+  
+      });
 
-      // converting the strings into array to get the time for now. 
-      const sunriseArray = localSunriseTime.split(" ")
-      const sunsetArray = localSunsetTime.split(" ")
-      console.log('local Sunrise Time with date: ', localSunriseTime);
-      console.log('local Sunrise Time with date: ', localSunsetTime);
-      console.log('sunset local time: ', sunsetArray);
-      
-      localSunriseTime =  sunriseArray[4]
-      localSunsetTime =  sunsetArray[4]
-      console.log('sunrise local time: ', localSunriseTime);
-      console.log('sunset local time: ', localSunsetTime);
-
-      let totalTime
-
-      
-      if(firstRun.pace ==="Jog" ){
-        if(firstRun.distance === '5km'){
-          totalTime = 37.5
-        }
-        if(firstRun.distance === '10km'){
-          totalTime = 75
-        }
-        if(firstRun.distance === 'Half Marathon'){
-          totalTime = 157.5
-        }
-        if(firstRun.distance === 'Marathon'){
-          totalTime = 315
-        }
-        // firstRun.distance
-      }
-      if(firstRun.pace ==="Run"){
-        if(firstRun.distance === '5km'){
-          totalTime = 18.75
-        }
-        if(firstRun.distance === '10km'){
-          totalTime = 37.5
-        }
-        if(firstRun.distance === 'Half Marathon'){
-          totalTime = 78.75
-        }
-        if(firstRun.distance === 'Marathon'){
-          totalTime = 157.5
-        }
-      }
-      let timeInMinutes
-      if(firstRun.userSunrise){
-        timeInMinutes = convertH2M(localSunriseTime);
-      }else if(firstRun.userSunset){
-        timeInMinutes = convertH2M(localSunsetTime);
-      }
-
-      const startTime = Number(timeInMinutes) -(totalTime/2)
-      let startMinute = Math.floor(startTime % 60)
-      let startHour = (Math.floor(startTime / 60 ))
-      if(startMinute < 10){
-        startMinute = `0${startMinute}`
-      }
-      let convertingToHourMinute
-      if(startHour > 12){
-        convertingToHourMinute = (`${startHour-12}:${startMinute} PM`)
-      }else {
-        convertingToHourMinute = (`${startHour}:${startMinute} AM`)
-      }
-
-      localSunriseTime =  sunriseArray[4]
-      localSunsetTime =  sunsetArray[4]  
-      setRunResults({...firstRun, sunsetData: localSunsetTime, sunriseData:localSunriseTime, departureTime: convertingToHourMinute });   
-
-    });
-    function convertH2M(timeInHour){
-      var timeParts = timeInHour.split(":");
-      return Number(timeParts[0]) * 60 + Number(timeParts[1]);
     }
+    
+  }
+  function convertH2M(timeInHour){
+    var timeParts = timeInHour.split(":");
+    return Number(timeParts[0]) * 60 + Number(timeParts[1]);
   }
   return (
     <section>
@@ -181,12 +178,16 @@ function RunSetup() {
 
       <div className='runResults'>
         <h3>Here's what we got for you:</h3>
+        {
+          alert.alert === true ? <p style={{color: "red"}}>{alert.alertMessage}</p> : null
+        }
 
         {
           runResults ?
           <>
           {runResults.userSunrise === true ? 
           <div>
+            <p>Date: {runResults.date}</p>
             <p>Sunrise: {runResults.sunriseData}</p> 
             <p>Departure Time:  {runResults.departureTime}</p>
 
@@ -199,6 +200,7 @@ function RunSetup() {
            : null} */}
           {runResults.userSunset === true ? 
           <div>
+            <p>Date: {runResults.date}</p>
             <p>Sunset: {runResults.sunsetData}</p>
             <p>Departure Time:  {runResults.departureTime}</p>
 
