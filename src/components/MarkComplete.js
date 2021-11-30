9f30b5b2c94837ff839d0440fedd28bebecaf12d
dallanj <dallanjones@pm.me>
2021-11-30 18:29:27 -0500

    finished mark run complete component, added error handling, functionality, validation and css

--- src/components/MarkComplete.js
+++ src/components/MarkComplete.js
@@ -1,40 +1,28 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
 import firebase from '../firebase';
 
 
 function MarkComplete(props) {
 
-        // function to remove run from database
-        function markComplete(markComplete) {
-            
-            const user = props.userInfo
-            // if theres is a key property of runs then run the following functions
-            if(user.runs){
-                // copy the old array
-                // let oldRunArray = [...user.runs]
-                // filter the run that needs to be deleted by returning the elements whose id is not equal to the selected element
-                // const filteredArray = oldRunArray.filter(run=> run.id !== removeItemId)
-                // creating an object with the filtered array.
-                // const runObj = {
-                //     runs: filteredArray
-                // }
-                
-                // rerendering the runs in the dom with the new array.
-                // props.runReRender(filteredArray)
+    // function to mark user's run complete
+    const markRunComplete = () => {
 
-                // set 'completed' to be true
-                const mark = {completed:true}
+        // make db connection
+        const dbRef = firebase.database().ref(`/sample/${props.userId}/runs/${props.runKey}`);
 
-                // updating the runs array in the firebase with the new filtered array of runs.
-                firebase.database().ref(`/sample/${props.userId}/runs/${props.run}`).update(mark);
-                
-            }else{
-                console.log('nothing to mark complete')
-            }
-        }
+        // set 'completed' to be true
+        const mark = {completed:true}
+        
+        // push mark obj to db
+        dbRef.update(mark);
+
+        // close the modal
+        props.closeModal();
+
+    }
+
     return (
-        <button aria-label="mark run complete" className="runs-item" onClick={() => markComplete(props.run)}>
+        <button aria-label="mark run complete" className="runs-item" onClick={() => markRunComplete()}>
             <i class="fas fa-check-circle"></i>
         </button>
     )
--- src/components/UpcomingRuns.js
+++ src/components/UpcomingRuns.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
 import firebase from '../firebase';
-import {useParams} from 'react-router-dom'
+import {useParams , useNavigate} from 'react-router-dom'
+import raw from '../includes/list.txt'; // profanity list! >:)
 
 // import components
 import MarkComplete from './MarkComplete';
@@ -12,6 +13,8 @@ import '../modal.css'
 
 
 function UpcomingRuns() {
+    let navigate = useNavigate();
+
 
     // useState declarations
     const [userRuns, setUserRuns] = useState([]) // initial runs state, has both incomplete and complete
@@ -25,6 +28,12 @@ function UpcomingRuns() {
     const [runKey, setRunKey] = useState('');
     const [runObj, setRunObj] = useState([]);
     const [note, setNote] = useState('');
+
+    const [runObjForModal, setRunObjForModal]=useState({})
+
+    // note validity check
+    const [isNoteValid, setIsNoteValid] = useState(0); // added by dallan
+    const [didNoteUpdate, setDidNoteUpdate] = useState(0);
 
     // get userId from url and store in userId
     const user = useParams()
@@ -71,11 +80,17 @@ function UpcomingRuns() {
     function runModal(runId) {
         setModal(true);
         setRunId(runId);
+        userRuns.forEach((run, idx)=>{
+            if(run.id === runId){
+                console.log('runObj', run)
+                console.log('run id', idx)
+                setRunObjForModal(run)
+            }
+        })
+        for(let i=0; i<userRuns.length;i++) {
+            console.log(userRuns[i].id)
 
-        for(let i=0; i<incompleteRuns.length;i++) {
-            console.log(incompleteRuns[i].id)
-
-            if(incompleteRuns[i].id === runId) {
+            if(userRuns[i].id === runId) {
                 console.log('im here')
                 setRunKey(i);
             }
@@ -85,17 +100,89 @@ function UpcomingRuns() {
     function closeModal() {
         setModal(false);
         setRunId('')
+
+        // reset note validation messages
+        setIsNoteValid(0)
+        setDidNoteUpdate(0)
     }
 
     // function to add a note to your run
-    const addNote = (event) => {
+    const addNote = (event) => { // by dallan
         event.preventDefault();
 
-        // make db connection
-        const dbRef = firebase.database().ref(`/sample/${user.userId}/runs/${runKey}`);
+        // fetch the imported profanity list >:) by dallan
+        fetch(raw)
+        .then(r => r.text())
+        .then(text => {
+      
+            // create an items array which adds each word by new line
+            const keywords = text.split("\n");
+        
+            // remove empty word at bottom of array
+            keywords.splice(452, 1);
 
-        // push note to db
-        dbRef.update(note);
+            // regex for finding specific substring
+            const regex = new RegExp('\\b('+keywords.join('|')+')\\b','i');
+            
+            // function finds if a string contains a value, not case sensitive, from array of words
+            function anyValueMatches( o, r ) {
+                // if a word matches any keyword return true
+                for( const k in o ) if( r.test(o[k]) ) return true;
+
+                // if no keyword is found return false
+                return false;
+            }
+
+            let noteString = note.note;
+            
+            if(noteString === undefined) {
+                noteString = '';
+            }
+            let errorCheck = 'false';
+            // set isNoteValid to 1 if note contains profanity
+            if(anyValueMatches(note,regex)) { 
+                setIsNoteValid(1);
+                setDidNoteUpdate(0);
+
+                setTimeout(function(){
+                    setIsNoteValid(0);
+                }, 7000);
+
+                console.log('noteisvalid: ', isNoteValid)
+                errorCheck = 'true';
+            }
+
+            // set isNoteValid to 2 if note has more than 250 characters
+            if(noteString.length > 250) {
+                setIsNoteValid(2);
+                setDidNoteUpdate(0);
+
+                setTimeout(function(){
+                    setIsNoteValid(0);
+                }, 9000);
+
+                console.log('noteisvalid: ', isNoteValid)
+                errorCheck = 'true';
+            }
+
+            // if isNoteValid === 0 then add note to database
+            if(errorCheck === 'false') {
+                // make db connection
+                const dbRef = firebase.database().ref(`/sample/${user.userId}/runs/${runKey}`);
+
+                // push note to db
+                if(dbRef.update(note)) {
+                    setDidNoteUpdate(1);
+                    setIsNoteValid(0);
+
+                    setTimeout(function(){
+                        setDidNoteUpdate(0)
+                    }, 9000);
+                }
+            }
+
+        })
+  
     }
 
     // handle note change
@@ -104,18 +191,18 @@ function UpcomingRuns() {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-    
+
         // set the data to note state
         setNote({
             ...note,
             [name]: value,
         })
         
+              
     }
 
     // function to add a note to your run
     const markRunComplete = () => {
-
 
         // make db connection
         const dbRef = firebase.database().ref(`/sample/${user.userId}/runs/${runKey}`);
@@ -125,49 +212,41 @@ function UpcomingRuns() {
         
         // push mark obj to db
         dbRef.update(mark);
-        console.log('runKey', runKey)
 
+        // close the modal
         closeModal()
 
+    }
 
-        incompleteRuns.forEach(run => {
-            console.log(run)
-            console.log('runkey ', runKey)
-        })
-        
-
-        console.log(incompleteRuns)
+    const editRun =(runObj)=>{
+        console.log(runObj)
+        navigate(`/setup/${user.userId}/${runObj.id}`);
 
     }
 
     return (
         <>
 
-   
-
-
             <h3>Upcoming runs</h3>
-            <div className="flex-container">
-
-            
+            <div className="flex-container">            
             {/* list the user's upcoming runs */}
             <div>
                 {
                     // using map to iterate through userRuns array
-                    incompleteRuns.map((run) => {
-                        return(
-                            <div className="runs-panel" key={run.id}>
-                                {/* <Link key={user.userId} to={`/run/${run.id}`}> */}
-                                    <button className="runs-item" onClick={() => runModal(run.id)}>
-                                        You have a run on {run.date} <i class="fas fa-ellipsis-h"></i>
-                                    </button>
-
-                                    {/* option to remove run (will place in dropdown menu) */}
-                                    <DeleteRun run={run} userId={user.userId} userInfo={userInfo} runReRender={setIncompleteRuns}/> {/* added by 😈sara  */}
-                                    <EditRun run={run} userId={user.userId} />
-                            
-                            </div>
-                        )
+                    userRuns.map((run) => {
+                        if(run.completed===false) {
+                            return(
+                                <div className="runs-panel" key={run.id}>
+                                    {/* <Link key={user.userId} to={`/run/${run.id}`}> */}
+                                        <button className="runs-item" onClick={() => runModal(run.id)}>
+                                            You have a run on {run.date} <i class="fas fa-ellipsis-h"></i>
+                                        </button>
+                                        {/* option to remove run (will place in dropdown menu) */}
+                                        {/* <DeleteRun run={run} userId={user.userId} userInfo={userInfo} runReRender={setIncompleteRuns}/> */}
+                                        {/* added by 😈sara  */}
+                                </div>
+                            )
+                        }
                     })
 
                 }
@@ -179,25 +258,14 @@ function UpcomingRuns() {
             <div>
                 {
                     // using map to iterate through userRuns array
-                    completedRuns.map((run) => {
-                        return(
-                            <div key={run.id}>
-                                <p>You have completed the run that was on {run.date}</p>
-                                    {/* {
-                                        // check if run is completed
-                                        run.completed ?
-                                        (
-                                            <span> true</span>
-                                            
-                                        ) :
-                                        (
-                                            <span> false</span>
-                                        )
-                                    } */}
-                            </div>
-
-                            
-                        )
+                    userRuns.map((run) => {
+                        if(run.completed === true) {
+                            return(
+                                <div key={run.id}>
+                                    <p>You have completed the run that was on {run.date}</p>
+                                </div> 
+                            )
+                        }
                     })
 
                 }
@@ -207,8 +275,9 @@ function UpcomingRuns() {
             {
                 modal === true ? (
                     <>
-                        {/* set modal overlay overtop of entire page */}
-                        <div className="modal-overlay" onClick={() => closeModal()}></div>
+                    {/* set modal overlay overtop of entire page */}
+                    <div className="modal-overlay" onClick={() => closeModal()}></div>
+
 
                         {/* set modal card overtop of overlay */}
                         <div className="modal-card">
@@ -216,42 +285,43 @@ function UpcomingRuns() {
                             <div className="modal-grid modal-title">
                                 <h3>Run information</h3>
                                 <div className="modal-options">
-
                                     {/* mark run complete button */}
-                                    {/* <button aria-label="mark run complete" onClick={() => markRunComplete()}>
-                                        <i class="fas fa-check-circle"></i>
-                                    </button> */}
                                     <MarkComplete 
-                                        run={runKey} 
+                                        runKey={runKey} 
                                         userId={user.userId} 
-                                        userInfo={userInfo} 
-                                        runReRender={setIncompleteRuns}
+                                        closeModal={closeModal}
                                     />
 
                                     {/* edit the run settings */}
-                                    <button aria-label="edit the run settings">
+                                    <button aria-label="edit the run settings" onClick={()=>editRun(runObjForModal)}>
                                         <i class="fas fa-edit"></i>
                                     </button>
 
                                     {/* delete the run */}
-                                    <button aria-label="delete the run">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-
+                                    <DeleteRun run={runObjForModal} setRunKey={setRunKey} userId={user.userId} userInfo={userInfo} runReRender={setIncompleteRuns} closeModal={closeModal}/> {/* added by 😈sara  */}
                                 </div>
                             </div>
 
                             <div className="modal-grid modal-content">
 
-                            {/* <p>Run id: {incompleteRuns[runKey].id}</p> */}
-                            <p>Date: {incompleteRuns[runKey].date}</p>
-                            <p>Departure time: {incompleteRuns[runKey].departureTime}</p>
-                            <p>Distance: {incompleteRuns[runKey].distance}</p>
-                            <p>Pace: {incompleteRuns[runKey].pace}</p>
-                            <p>Duration: {incompleteRuns[runKey].runDuration}</p>
-                            <p>Sun time: {incompleteRuns[runKey].suntime}</p>
-                            <p>Time of Day: {incompleteRuns[runKey].timeOfDay}</p>
+                                {/* <p>Run id: {incompleteRuns[runKey].id}</p> */}
+                                {/* <p>Date: {incompleteRuns[runKey].date}</p>
+                                <p>Departure time: {incompleteRuns[runKey].departureTime}</p>
+                                <p>Distance: {incompleteRuns[runKey].distance}</p>
+                                <p>Pace: {incompleteRuns[runKey].pace}</p>
+                                <p>Duration: {incompleteRuns[runKey].runDuration}</p>
+                                <p>Sun time: {incompleteRuns[runKey].suntime}</p>
+                                <p>Time of Day: {incompleteRuns[runKey].timeOfDay}</p> */}
 
+
+                                {/* sara  😈 sara */}
+                                <p>Date: {runObjForModal.date}</p>
+                                <p>Departure time: {runObjForModal.departureTime}</p>
+                                <p>Distance: {runObjForModal.distance}</p>
+                                <p>Pace: {runObjForModal.pace}</p>
+                                <p>Duration: {runObjForModal.runDuration}</p>
+                                <p>Sun time: {runObjForModal.suntime}</p>
+                                <p>Time of Day: {runObjForModal.timeOfDay}</p>
                             </div>
                             
 
@@ -262,9 +332,16 @@ function UpcomingRuns() {
 
                             {/* add sr-only label to textarea */}
                             <form className="modal-notepad-form" aria-label="Display income data with a remove income option" onSubmit={addNote}>
-                                <textarea className="modal-notepad" name="note" id="note" onChange={handleNoteChange}>{incompleteRuns[runKey].note}</textarea>
+                                
+                                {didNoteUpdate === 1 && <div className="modal-msg modal-success">Success! You've updated your notes.</div> }
+                                {isNoteValid === 1 && <div className="modal-msg modal-error">Watch the language there chief.</div> }
+                                {isNoteValid === 2 && <div className="modal-msg modal-error">The note cannot be more than 250 characters long! </div> }
+                                
+                                {/* <textarea className="modal-notepad" name="note" id="note" onChange={handleNoteChange}>{incompleteRuns[runKey].note}</textarea> */}
+                                <textarea className="modal-notepad" name="note" id="note" onChange={handleNoteChange}>{runObjForModal.note}</textarea>
 
                                 <button aria-label="Add note" className="btn-green">Update notes</button>
+                            
                             </form>
 
 
